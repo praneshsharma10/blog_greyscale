d7c3d9579d99e3dd22a729bb5b215ae7b1cd896c
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2025-01-16 18:40:18 +0530

    contact header

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import { Menu, X } from 'lucide-react'
 const navigation = [
   { name: 'Home', href: '/' },
   { name: 'Blogs', href: '/blogs' },
-
+  { name: 'Contact', href: '/contact' },
 ]
 
 export function Header() {
