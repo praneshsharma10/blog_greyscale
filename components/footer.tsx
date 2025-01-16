27cfd373baf54467a9d1d73ed94e00b0e4364bf0
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2025-01-17 01:23:35 +0530

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,17 @@
 
 
 export function Footer() {
-  return (
-    <footer className="">
-      
-    </footer>
-  )
-}
+    return (
+      <footer className="border-t bg-gray-50">
+        <div className=" px-4 py-12 ">
+          
+          
+        </div>
+      </footer>
+    )
+  }
+  
+  
+
+
 
