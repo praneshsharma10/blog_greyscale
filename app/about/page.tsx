7965f97446c39f7d2d36d9d7dad4653925fac086
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2025-01-17 01:30:49 +0530

    abt

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,36 @@
 
 export default function AboutPage() {
     return (
-      <div>
+      <div className="">
+        <div className="">
+            <div className="">
+                <h1 className="font-bold text-2xl text-black justify-center	">
+                    about
+                </h1>
+                <div>
+                    <p>
+                        HI
+
+                    </p>
+                    <h2>
+
+                    </h2>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+
+                </div>
+                
+                {/* icons */}
+                <div>
+
+                </div>
+                
+            </div>
+        </div>
           
       </div>
         )
   }
-  
   
