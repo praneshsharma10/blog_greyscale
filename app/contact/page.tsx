27136044f1d0de0ba4554f5e10337fb0bccd19fd
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2025-01-16 18:39:11 +0530

    contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function ContactPage() {
+  return (
+    <div className="">
+      <div className="">
+        <h1 className="">
+          contact
+        </h1>
+        
+        
+      </div>
+    </div>
+  )
+}
+
