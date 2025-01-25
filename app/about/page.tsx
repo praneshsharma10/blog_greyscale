2a50e6c3643ec76fc98e01bcbcbf3bc2a363762c
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2025-01-25 22:34:55 +0530

    about page basic structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,13 @@
-import { Github } from "lucide-react";
+import { Github , Linkedin , Twitter } from "lucide-react";
 import Link from "next/link";
 
 
 export default function AboutPage() {
     return (
-      <div className="">
-        <div className="">
+      <div className="mx-auto max-w-7xl px-6 py-12 lg:px-8">
+        <div className="grid gap-12 md:grid-cols-2">
             <div className="">
-                <h1 className="font-bold text-2xl text-black justify-center	">
+                <h1 className="font-bold text-2xl text-black justify-center mb-5">
                     about
                 </h1>
                 <div>
@@ -22,16 +22,23 @@ export default function AboutPage() {
                     <ul>
                         <li>AWS</li>
                         <li>git</li>
+                        <li>Devops</li>
+
                     </ul>
 
                 </div>
                 
                 {/* icons */}
-                <div className="flex gap-4">
-                    <Link href="">
-                        <Github className="" size={24} />
+                <div className="flex gap-6 mt-8">
+                    <Link href="https://github.com" className="text-gray-500 hover:text-gray-900">
+                    <Github className="h-6 w-6" />
                     </Link>
-
+                    <Link href="https://linkedin.com" className="text-gray-500 hover:text-gray-900">
+                    <Linkedin className="h-6 w-6" />
+                    </Link>
+                    <Link href="https://twitter.com" className="text-gray-500 hover:text-gray-900">
+                    <Twitter className="h-6 w-6" />
+                    </Link>
                 </div>
                 
             </div>
